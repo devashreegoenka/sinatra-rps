f57f48c90e78ef4aba8f3c9f06e251704c4c6039
devashreegoenka <devashree@uchicago.edu>
2024-10-21 03:01:23 +0000

    changes to rock/paper/scissors

--- app.rb
+++ app.rb
@@ -2,8 +2,62 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+#code for rock 
+
+get("/rock") do 
+
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+if @comp_move == "rock"
+  @outcome = "We tied!"
+elsif @comp_move == "paper"
+  @outcome = "We won!"
+else 
+  @outcome =  "We lost!"
+end 
+
+erb(:rock)
+
+end
+
+#Code for scissors
+
+get("/scissors") do 
+
+  moves = ["rock", "paper", "scissors"]
+  comp_move = moves.sample
+
+  if @comp_move = "rock"
+    @outcome = "We lost!"
+  elsif @comp_move = "paper"
+    @outcome = "We tied!!"
+  else 
+    @outcome =  "We won!"
+  end
+
+  erb(:scissors)
+
+end
+
+#Code for paper
+
+get("/paper") do 
+
+  moves = ["rock", "paper", "scissors"]
+  comp_move = moves.sample
+
+  if @comp_move = "rock"
+    @outcome = "We won!"
+  elsif @comp_move = "paper"
+    @outcome = "We tied!"
+  else 
+    @outcome =  "We lost!"
+  end
+
+  erb(:paper)
+
 end
